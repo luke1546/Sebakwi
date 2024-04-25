cb676d7e4c28f65a1d72f542ff0c149602c2c880
신성현 <froggy0m0a@gmail.com>
2024-04-26 01:55:14 +0900

    [fix] test 어노테이션제거

--- sebakwi-be/src/test/java/com/ssafy/sebakwi/SebakwiApplicationTests.java
+++ sebakwi-be/src/test/java/com/ssafy/sebakwi/SebakwiApplicationTests.java
@@ -3,7 +3,7 @@ package com.ssafy.sebakwi;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+// @SpringBootTest
 class SebakwiApplicationTests {
 
 	@Test
