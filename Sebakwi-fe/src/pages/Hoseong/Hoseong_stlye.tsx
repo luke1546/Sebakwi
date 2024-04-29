3c4afcac6724d5dac28afd2426de688ed91f64db
hoseong <luke1546@naver.com>
2024-04-29 17:57:56 +0900

    feat: three js practice

--- Sebakwi-fe/src/pages/Hoseong/Hoseong_stlye.tsx
+++ Sebakwi-fe/src/pages/Hoseong/Hoseong_stlye.tsx
@@ -7,3 +7,35 @@ export const Title = styled.p`
   font-family: ${Noto_Sans_KR.regular.variable};
   font-weight: ${Noto_Sans_KR.regular.weight};
 `;
+
+export const Button = styled.button`
+display: inline-flex;
+align-items: center;
+outline: none;
+border: none;
+border-radius: 4px;
+color: white;
+font-weight: bold;
+cursor: pointer;
+padding-left: 1rem;
+padding-right: 1rem;
+
+
+/*크기*/
+height: 2.25rem;
+font-size: 1rem;
+
+/*색상 */
+background: #228be6;
+&:hover{
+    background: #339af0;
+}
+&:active{
+    background: #1c7ed6;
+}
+
+/*기타 */
+& + & {
+    margin-left: 1rem;
+}
+`;
