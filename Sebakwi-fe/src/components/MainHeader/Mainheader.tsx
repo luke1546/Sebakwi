cd7df1b133a453e489061a9678df526a579e1a42
오지훈 <gfsd2397@naver.com>
2024-04-29 10:31:52 +0900

    [chore] 24.04.29 폴더 및 디렉토리 네이밍 수정

---
+++ Sebakwi-fe/src/components/MainHeader/Mainheader.tsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import { Link, useLocation } from 'react-router-dom';
+import * as Styled from './MainHeader_style';
+
+export default function Mainheader() {
+  const location = useLocation();
+
+  return (
+    <Styled.MainheaderContainer>
+      <Styled.LogoWrapper>
+        <Link to="/">
+          <img src="/images/semesLogo.png" alt="Logo" width={120} height={28} />
+        </Link>
+      </Styled.LogoWrapper>
+      <Styled.StyledLink to="/" isActive={location.pathname === '/'}>
+        대시보드
+      </Styled.StyledLink>
+      <Styled.StyledLink
+        to="/checkupwheels"
+        isActive={location.pathname === '/checkupwheels'}
+      >
+        검진 목록
+      </Styled.StyledLink>
+      <Styled.StyledLink
+        to="/statistics"
+        isActive={location.pathname === '/statistics'}
+      >
+        통계
+      </Styled.StyledLink>
+      <Styled.StyledLink to="/jihun" isActive={location.pathname === '/jihun'}>
+        jihun
+      </Styled.StyledLink>
+      <Styled.StyledLink to="/jiwon" isActive={location.pathname === '/jiwon'}>
+        jiwon
+      </Styled.StyledLink>
+      <Styled.StyledLink
+        to="/hoseong"
+        isActive={location.pathname === '/hoseong'}
+      >
+        hoseong
+      </Styled.StyledLink>
+
+      <Styled.StyledLink
+        to="/example"
+        isActive={location.pathname === '/example'}
+      >
+        Example
+      </Styled.StyledLink>
+    </Styled.MainheaderContainer>
+  );
+}
+//
