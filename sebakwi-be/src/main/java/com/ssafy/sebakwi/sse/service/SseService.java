724b0f7b3ff1dc6cf73fb49b19d8927796135371
Jinsung Lee <kd0201e07@gmail.com>
2024-05-13 21:44:23 +0900

    fix: sseExceptionResponse처리

--- sebakwi-be/src/main/java/com/ssafy/sebakwi/sse/service/SseService.java
+++ sebakwi-be/src/main/java/com/ssafy/sebakwi/sse/service/SseService.java
@@ -2,23 +2,31 @@ package com.ssafy.sebakwi.sse.service;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ssafy.sebakwi.sse.domain.EmitterRepository;
+import com.ssafy.sebakwi.util.dto.ErrorResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.ToString;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 import java.io.IOException;
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Slf4j
 @RequiredArgsConstructor
 @Service
+@ToString
 public class SseService {
 
     private static final Long DEFAULT_TIMEOUT = 60L * 1000 * 60;
 
     private final EmitterRepository emitterRepository;
+
+    private final ThreadPoolTaskExecutor taskExecutor;
 
     @Autowired
     private ObjectMapper objectMapper;
@@ -27,7 +35,7 @@ public class SseService {
 
         UUID uuid = UUID.randomUUID();
         SseEmitter emitter = createEmitter(uuid);
-        sendToClient(uuid, "안녕하세요 연결 됐습니다");
+        sendToClient(uuid, "연결되었습니다 \uD83D\uDE80");
 
         return emitter;
     }
@@ -49,7 +57,24 @@ public class SseService {
                 emitter.send(SseEmitter.event().id(String.valueOf(uuid)).name("sse").data(jsonData));
             } catch (IOException e) {
                 emitterRepository.deleteById(uuid);
-                emitter.completeWithError(e);
+//                emitter.completeWithError(e);
+                emitter.complete();
+
+                //ErrorResponse 전송을 ExecutorService 에서 처리
+                taskExecutor.execute(() -> {
+                    try {
+                        ErrorResponse errorResponse = ErrorResponse.builder()
+                                .errorCode(9002)
+                                .message(e.getMessage())
+                                .errorTimestamp(LocalDateTime.now())
+                                .build();
+                        emitter.send(errorResponse, MediaType.parseMediaType("text/event-stream"));
+
+                    } catch (IOException ex) {
+                        log.info("sseResponseException={}", ex);
+                    }
+                });
+
             }
         }
     }
