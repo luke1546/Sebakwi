417d8e2618516746dfcf822553be1208a177d141
hoseong <luke1546@naver.com>
2024-05-13 14:31:30 +0900

    [feat] 24.05.13 바퀴 갯수 4개로 변경 and build

--- sebakwi-unity/Assets/CanvasManager.cs
+++ sebakwi-unity/Assets/CanvasManager.cs
@@ -5,9 +5,18 @@ using TMPro;
 
 public class CanvasManager : MonoBehaviour
 {
-    public Image greenBox;
-    public Image redBox;
-    public TMP_Text judgment;
+    public Image greenBox1;
+    public Image redBox1;
+    public Image greenBox2;
+    public Image redBox2;
+    public Image greenBox3;
+    public Image redBox3;
+    public Image greenBox4;
+    public Image redBox4;
+    public TMP_Text detect1;
+    public TMP_Text detect2;
+    public TMP_Text detect3;
+    public TMP_Text detect4;
     float time = -1.8f;
     void Start()
     {
@@ -30,29 +39,96 @@ public class CanvasManager : MonoBehaviour
 
     private void Show()
     {
-        string msg;
-        int randomNumber = Random.Range(0, 100);
-        if (randomNumber < 70)
+        string msg1;
+        string msg2;
+        string msg3;
+        string msg4;
+        int randomNumber1 = Random.Range(0, 100);
+        int randomNumber2 = Random.Range(0, 100);
+        int randomNumber3 = Random.Range(0, 100);
+        int randomNumber4 = Random.Range(0, 100);
+        if (randomNumber1 < 70)
         {
-            msg = "Good ";
-            greenBox.gameObject.SetActive(true); // 대화 상자 활성화
+            msg1 = "Good ";
+            greenBox1.gameObject.SetActive(true); // 대화 상자 활성화
         }
         else
         {
-            msg = "Bad ";
-            redBox.gameObject.SetActive(true); // 대화 상자 활성화
+            msg1 = "Bad ";
+            redBox1.gameObject.SetActive(true); // 대화 상자 활성화
         }
-        float randomValue = Random.Range(0.5f, 0.99f);
-        msg += randomValue.ToString("F2");
-        judgment.text = msg;
-        judgment.gameObject.SetActive(true);
+
+        if (randomNumber2 < 70)
+        {
+            msg2 = "Good ";
+            greenBox2.gameObject.SetActive(true); // 대화 상자 활성화
+        }
+        else
+        {
+            msg2 = "Bad ";
+            redBox2.gameObject.SetActive(true); // 대화 상자 활성화
+        }
+
+        if (randomNumber3 < 70)
+        {
+            msg3 = "Good ";
+            greenBox3.gameObject.SetActive(true); // 대화 상자 활성화
+        }
+        else
+        {
+            msg3 = "Bad ";
+            redBox3.gameObject.SetActive(true); // 대화 상자 활성화
+        }
+
+        if (randomNumber4 < 70)
+        {
+            msg4 = "Good ";
+            greenBox4.gameObject.SetActive(true); // 대화 상자 활성화
+        }
+        else
+        {
+            msg4 = "Bad ";
+            redBox4.gameObject.SetActive(true); // 대화 상자 활성화
+        }
+
+        float randomValue1 = Random.Range(0.5f, 0.99f);
+        float randomValue2 = Random.Range(0.5f, 0.99f);
+        float randomValue3 = Random.Range(0.5f, 0.99f);
+        float randomValue4 = Random.Range(0.5f, 0.99f);
+        msg1 += randomValue1.ToString("F2");
+        detect1.text = msg1;
+        detect1.gameObject.SetActive(true);
+
+        msg2 += randomValue2.ToString("F2");
+        detect2.text = msg2;
+        detect2.gameObject.SetActive(true);
+
+        msg3 += randomValue3.ToString("F2");
+        detect3.text = msg3;
+        detect3.gameObject.SetActive(true);
+
+        msg4 += randomValue4.ToString("F2");
+        detect4.text = msg4;
+        detect4.gameObject.SetActive(true);
     }
 
     // 대화 상자 숨기기
     private void Hide()
     {
-        greenBox.gameObject.SetActive(false); // 대화 상자 비활성화
-        redBox.gameObject.SetActive(false); // 대화 상자 비활성화
-        judgment.gameObject.SetActive(false);
+        greenBox1.gameObject.SetActive(false); // 대화 상자 비활성화
+        redBox1.gameObject.SetActive(false); // 대화 상자 비활성화
+        detect1.gameObject.SetActive(false);
+
+        greenBox2.gameObject.SetActive(false); // 대화 상자 비활성화
+        redBox2.gameObject.SetActive(false); // 대화 상자 비활성화
+        detect2.gameObject.SetActive(false);
+
+        greenBox3.gameObject.SetActive(false); // 대화 상자 비활성화
+        redBox3.gameObject.SetActive(false); // 대화 상자 비활성화
+        detect3.gameObject.SetActive(false);
+
+        greenBox4.gameObject.SetActive(false); // 대화 상자 비활성화
+        redBox4.gameObject.SetActive(false); // 대화 상자 비활성화
+        detect4.gameObject.SetActive(false);
     }
 }
